0ac17a780327c0bb9cc0391130555788ff0ad151
Irina060300 <90128047+Irina060300@users.noreply.github.com>
2022-08-19 19:36:04 +0300

    Update input_data.c

--- input_data.c
+++ input_data.c
@@ -1,54 +1,62 @@
 #include "header.h"
 
-void init_signes(signes *stack) {
-    stack -> top = 0;
-}
-void init_numbers(numbers *stack) {
-    stack -> top = 0;
-}
+void (*change_funcs[])(char *) = {
+    change_unar_minus,
+    change_sqrt,
+    change_sin,
+    change_cos,
+    change_ctg,
+    change_tg,
+    change_ln,
+    NULL
+};
 
+void init_signes(t_signes *stack) {
+    stack->top = 0;
+}
+void init_numbers(t_numbers *stack) {
+    stack->top = 0;
+}
 void input(char *data) {
+    printf("  ");
     scanf("%[^\n]%*c", data);
 }
-void push_signes(signes *stack, char c) {
+void push_signes(t_signes *stack, char c) {
     stack->sig[stack->top] = c;
     stack->top++;
 }
-void push_numbers(numbers *stack, double c) {
+void push_numbers(t_numbers *stack, double c) {
     stack->num[stack->top] = c;
     stack->top++;
 }
 
-char pop_signes(signes *stack) {
+char pop_signes(t_signes *stack) {
     if (stack->top > 0) {
         stack->top--;
     }
     return stack->sig[stack->top];
 }
-double pop_numbers(numbers *stack) {
-    if (stack->top > 0) {
+
+double pop_numbers(t_numbers *stack) {
+    if (stack->top >= 0) {
         stack->top--;
     }
     return stack->num[stack->top];
 }
 void change_str(char *data, char *new_data) {
-    change_unar_minus(data);
-    change_sqrt(data);
-    change_sin(data);
-    change_cos(data);
-    change_ctg(data);
-    change_tg(data);
-    change_ln(data);
+    for (int i = 0; change_funcs[i] != NULL; i++) {
+        change_funcs[i](data);
+    }
     delete_spaces(data, new_data);
-
 }
+
 void change_unar_minus(char *data) {
     if (data[0] == '-') {
         data[0] = '~';
     }
     size_t i;
-    for (i = 1; i < strlen(data) - 1; i++) {
-        if (data[i] == '(' && data [i+1] == '-') {
+    for (i = 0; i < strlen(data) - 1; i++) {
+        if (data[i] == '(' && data[i + 1] == '-') {
             data[i + 1] = '~';
         }
     }
@@ -78,7 +86,7 @@ void change_sin(char *data) {
         if (data[i + 1] == 'h') {
             data[i + 1] = '!';
         }
-    } 
+    }
     if (strlen(data) == 2) {
         if (data[0] == 'h') {
             data[0] = '!';
@@ -86,8 +94,8 @@ void change_sin(char *data) {
         if (data[1] == 'h') {
             data[1] = '!';
         }
-    } 
-    if (strlen(data) == 1){
+    }
+    if (strlen(data) == 1) {
         if (data[0] == 'h') {
             data[0] = '!';
         }
@@ -117,7 +125,7 @@ void change_cos(char *data) {
         if (data[i + 1] == 'p') {
             data[i + 1] = '!';
         }
-    } 
+    }
     if (strlen(data) == 2) {
         if (data[0] == 'p') {
             data[0] = '!';
@@ -125,8 +133,8 @@ void change_cos(char *data) {
         if (data[1] == 'p') {
             data[1] = '!';
         }
-    } 
-    if (strlen(data) == 1){
+    }
+    if (strlen(data) == 1) {
         if (data[0] == 'p') {
             data[0] = '!';
         }
@@ -141,7 +149,6 @@ void change_tg(char *data) {
                 data[i + 1] = ' ';
             }
         }
-        data[i + 1] = '\0';
         for (i = 0; i < strlen(data) - 1; i++) {
             if (data[i] == 't') {
                 if (!(data[i + 1] == ' ')) {
@@ -169,7 +176,6 @@ void change_ctg(char *data) {
                 data[i + 2] = ' ';
             }
         }
-        data[i + 2] = '\0';
         for (i = 0; i < strlen(data) - 2; i++) {
             if (data[i] == 'k') {
                 if (!(data[i + 1] == ' ' && data[i + 2] == ' ')) {
@@ -183,7 +189,7 @@ void change_ctg(char *data) {
         if (data[i + 1] == 'k') {
             data[i + 1] = '!';
         }
-    } 
+    }
     if (strlen(data) == 2) {
         if (data[0] == 'k') {
             data[0] = '!';
@@ -191,8 +197,8 @@ void change_ctg(char *data) {
         if (data[1] == 'k') {
             data[1] = '!';
         }
-    } 
-    if (strlen(data) == 1){
+    }
+    if (strlen(data) == 1) {
         if (data[0] == 'k') {
             data[0] = '!';
         }
@@ -209,10 +215,9 @@ void change_sqrt(char *data) {
                 data[i + 3] = ' ';
             }
         }
-        data[i + 3] = '\0';
         for (i = 0; i < strlen(data) - 3; i++) {
             if (data[i] == 'q') {
-                if (!(data[i + 1] == ' ' && data[i + 2] == ' '  && data[i + 3] == ' ')) {
+                if (!(data[i + 1] == ' ' && data[i + 2] == ' ' && data[i + 3] == ' ')) {
                     data[i] = '!';
                 }
             }
@@ -237,7 +242,7 @@ void change_sqrt(char *data) {
         if (data[2] == 'q') {
             data[2] = '!';
         }
-    } 
+    }
     if (strlen(data) == 2) {
         if (data[0] == 'q') {
             data[0] = '!';
@@ -245,13 +250,12 @@ void change_sqrt(char *data) {
         if (data[1] == 'q') {
             data[1] = '!';
         }
-    } 
-    if (strlen(data) == 1){
+    }
+    if (strlen(data) == 1) {
         if (data[0] == 'q') {
             data[0] = '!';
         }
     }
-
 }
 void change_ln(char *data) {
     if (strlen(data) > 1) {
@@ -262,7 +266,6 @@ void change_ln(char *data) {
                 data[i + 1] = ' ';
             }
         }
-        data[i + 1] = '\0';
         for (i = 0; i < strlen(data) - 1; i++) {
             if (data[i] == 'l') {
                 if (!(data[i + 1] == ' ')) {
@@ -280,7 +283,7 @@ void change_ln(char *data) {
         }
     }
 }
-void delete_spaces (char *data, char *new_data) {
+void delete_spaces(char *data, char *new_data) {
     size_t len = strlen(data), i = 0, j = 0;
     for (i = 0; i < len; i++) {
         if (data[i] != ' ') {
